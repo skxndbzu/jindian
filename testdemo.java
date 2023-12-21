e84fb5c3fe7119b5b22f456e13ad97587ea20322
陈铭鹤 <615880048@qq.com>
2023-12-21 14:25:39 +0800

    第一次提交

--- testdemo.java
+++ testdemo.java
@@ -3,6 +3,6 @@
  * @version 1.0
  */public class testdemo {
     public static void main(String[] args) {
-        System.out.println("demo分支提交测试");
+        System.out.println("demo分支提交测试。");
     }
 }
